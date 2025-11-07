adc2127bfb4950cd21ec178084abca8ab8023b8d
ibustamantem <ibustamantem@iesrafaelalberti.es>
2025-11-07 20:09:29 +0100

    Primer commit, añado readme, gitignore y dos archivos py

---
+++ readme.md
@@ -0,0 +1,8 @@
+# Ejercicio Git - Entornos de Desarrollo
+
+Autor: Iván
+Fecha: 30/10/2025
+
+Este repositorio contiene:
+- saludo.py
+- suma.py
---
+++ suma.py
@@ -0,0 +1,3 @@
+a = float(input("Introduce el primer número: "))
+b = float(input("Introduce el segundo número: "))
+print(f"La suma de {a} + {b} = {a + b}")
